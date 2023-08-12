23753e9b13c23c012f35ca2068b0857ba21f1e8a
dennis-oconnell <dennis.pat.oconnell@gmail.com>
2023-08-12 16:09:09 -0700

    adjusted formatting, started to include text selection capacities
    adjusted formatting, started to include text selection capacities

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const image = document.getElementById('myImage'); // Replace with the ID of your image element
+    const image = document.getElementById('highlighted-image'); // Replace with the ID of your image element
+    const highlightBox = document.querySelector(".highlight-box");
   
+    image.addEventListener("mousemove", function (event) {
+      const x = event.clientX - image.getBoundingClientRect().left;
+      const y = event.clientY - image.getBoundingClientRect().top;
+
+      // Set the highlight box dimensions and position
+      highlightBox.style.left = x + "px";
+      highlightBox.style.top = y + "px";
+      highlightBox.style.width = "100px"; // Adjust as needed
+      highlightBox.style.height = "40px"; // Adjust as needed
+
+      // Display the highlight box
+      highlightBox.style.display = "block";
+    });
+
+    image.addEventListener("mouseleave", function () {
+        // Hide the highlight box when the mouse leaves the image
+        highlightBox.style.display = "none";
+    });
+
     Tesseract.recognize(
       image,
       'lat', // Language code for Latin.
